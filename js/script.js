d4c9f1e18a72f9d6191f6e44a87a6618bd99a1fa
Jean Gomes Martins <jeangomesmart@gmail.com>
2024-09-12 20:28:54 -0300

    12/09

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+
+    var bt_nr = '';
+
     $('.bt-nr').click(function() {
-          var bt_nr = $(this).text();
-    }
+        var n = $(this).text();
+
+        if (bt_nr !== '0') {
+                bt_nr = bt_nr.concat(n);
+        } else if (bt_nr[0] !== '0' && (bt_nr[1] !== '.')) {
+         bt_nr = bt_nr.concat(n);
+        } else {
+            if (bt_nr.length == 1 && n == '.') {
+                bt_nr = bt_nr.concat(n);
+                $(this).attr('disabled', 'true');
+            } else {
+                bt_nr = bt_nr.concat(n);
+            };
+        };   
+
+        $('#txt-display').val(bt_nr);
+    });
 
 });
